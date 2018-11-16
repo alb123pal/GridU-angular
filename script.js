1fdc3d523800c2071deb938e3b0d72950d17f90a
apalka <apalka@apalka-ws-krakow.hw.griddynamics.net>
2018-11-16 16:03:06 +0100

    init project

---
+++ script.js
@@ -0,0 +1,41 @@
+var isHiddenListArticles = true;
+var isHiddenFilterOptions = true;
+
+function showDialog() {
+    var listArticlesNode = document.getElementsByClassName('list-articles')[0];
+    isHiddenListArticles = !isHiddenListArticles;
+    if (!isHiddenListArticles) {
+        listArticlesNode.style.display = 'grid';
+        document.getElementsByClassName('page-wrapper-content__modal-button')[0].remove();
+    }
+}
+
+var showHideFilterOptions = function() {
+    isHiddenFilterOptions = !isHiddenFilterOptions;
+    if (isHiddenFilterOptions) {
+    
+        document.getElementsByClassName('filters-panel-wrapper-advanced')[0].style.display = 'none';
+    } else {
+        document.getElementsByClassName('filters-panel-wrapper-advanced')[0].style.display = 'grid';
+    }
+}
+
+var checkValidation = function(ref) {
+    if (ref.type === "text") {
+        if (ref.value.length > 5) {
+            ref.parentElement.classList.add('error');
+            ref.parentElement.children[2].src = '../assets/login-invalid.svg';
+        } else {
+            ref.parentElement.classList.remove('error');
+            ref.parentElement.children[2].src = '../assets/login-inactive.svg';
+        }
+    } else {
+        if (ref.value.length > 5) {
+            ref.parentElement.classList.add('error');
+            ref.parentElement.children[2].src = '../assets/password-invalid.svg';
+        } else {
+            ref.parentElement.classList.remove('error');
+            ref.parentElement.children[2].src = '../assets/password-inactive.svg';
+        }
+    }
+}
