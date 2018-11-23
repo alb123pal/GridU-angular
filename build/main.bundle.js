4d6ceb62089d5dd7d9fc50a48ae478c6e81ddcfe
apalka <apalka@apalka-ws-krakow.hw.griddynamics.net>
2018-11-23 16:17:09 +0100

    feature: add slider to product_customer

--- build/main.bundle.js
+++ build/main.bundle.js
@@ -1,1 +1,29 @@
 "use strict";
+
+var currentSlide = 0,
+    maxSlideElement = 2,
+    panelSlideNode = document.getElementsByClassName("panel-slider__slides");
+
+var nextImage = function nextImage() {
+  panelSlideNode[0].children[currentSlide].classList.remove('active');
+
+  if (currentSlide == maxSlideElement) {
+    currentSlide = 0;
+  } else {
+    currentSlide++;
+  }
+
+  panelSlideNode[0].children[currentSlide].classList.add('active');
+};
+
+var previousImage = function previousImage() {
+  panelSlideNode[0].children[currentSlide].classList.remove('active');
+
+  if (currentSlide == 0) {
+    currentSlide = maxSlideElement;
+  } else {
+    currentSlide--;
+  }
+
+  panelSlideNode[0].children[currentSlide].classList.add('active');
+};
--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
+var currentSlide = 0,
+    maxSlideElement = 2,
+    panelSlideNode = document.getElementsByClassName("panel-slider__slides");
 
+let nextImage = function () {
+    panelSlideNode[0].children[currentSlide].classList.remove('active');
+
+    if (currentSlide == maxSlideElement) {
+        currentSlide = 0
+    } else {
+        currentSlide++;
+    }
+
+    panelSlideNode[0].children[currentSlide].classList.add('active');
+};
+
+let previousImage = function () {
+    panelSlideNode[0].children[currentSlide].classList.remove('active');
+
+    if (currentSlide == 0) {
+        currentSlide = maxSlideElement
+    } else {
+        currentSlide--;
+    }
+    
+    panelSlideNode[0].children[currentSlide].classList.add('active');
+};
